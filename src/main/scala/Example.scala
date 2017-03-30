cc6c5f5ea77d5be9250aa3f8aac9039ae9c621b4
Yogesh Radhakrishnan <dev.radhakrishnan.yogesh@dev.bwater.com>
2017-03-30 19:54:00 -0400

    Stay on Target Excercise

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -10,3 +10,25 @@ import doodle.backend.StandardInterpreter._
 object Example {
   val image = circle(10) on circle(20) on circle(30)
 }
+
+object Excercise{
+  def filledRectangle(wid:Int, bre:Int,fColor:Color,lineW:Int,lColor:Color):Image = {
+     rectangle(wid,bre) fillColor fColor lineWidth lineW lineColor lColor
+  }
+  def filledCircle(r:Int,fColor:Color,lineW:Int,lColor:Color):Image = {
+     circle(r) fillColor fColor lineWidth lineW lineColor lColor
+  }
+
+val greenRec= filledRectangle(40,25,Color.green,1,Color.green)
+val brownRec = filledRectangle(20,5,Color.brown,1,Color.black)
+val blackRec = filledRectangle(5,20,Color.white,4,Color.black)
+
+val smallRedCir = filledCircle(5,Color.red,2,Color.black)
+val whiteCir = filledCircle(10,Color.white,2,Color.black)
+val bigRedCir = filledCircle(15,Color.red,2,Color.black)
+
+val image = (smallRedCir on whiteCir on bigRedCir) above
+blackRec above
+brownRec above
+greenRec
+}
