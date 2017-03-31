c00de03ad46a7590d88ddfc3cf40f3aa3f06f5af
Yogesh Radhakrishnan <dev.radhakrishnan.yogesh@dev.bwater.com>
2017-03-31 13:06:05 -0400

    Add factor to the image size

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -11,24 +11,31 @@ object Example {
   val image = circle(10) on circle(20) on circle(30)
 }
 
-object Excercise{
+object Excercise {
+
   def filledRectangle(wid:Int, bre:Int,fColor:Color,lineW:Int,lColor:Color):Image = {
      rectangle(wid,bre) fillColor fColor lineWidth lineW lineColor lColor
   }
+
   def filledCircle(r:Int,fColor:Color,lineW:Int,lColor:Color):Image = {
      circle(r) fillColor fColor lineWidth lineW lineColor lColor
   }
 
-val greenRec= filledRectangle(40,25,Color.green,1,Color.green)
-val brownRec = filledRectangle(20,5,Color.brown,1,Color.black)
-val blackRec = filledRectangle(5,20,Color.white,4,Color.black)
+  def main(args:Array[String])= {
+    val factor = 5
 
-val smallRedCir = filledCircle(5,Color.red,2,Color.black)
-val whiteCir = filledCircle(10,Color.white,2,Color.black)
-val bigRedCir = filledCircle(15,Color.red,2,Color.black)
+    val greenRec = filledRectangle(factor * 50, factor * 20, Color.green, factor * 1, Color.green)
+    val brownRec = filledRectangle(factor * 20, factor * 5, Color.brown, factor * 1, Color.black)
+    val blackRec = filledRectangle(factor * 5, factor * 20, Color.white, factor * 4, Color.black)
 
-val image = (smallRedCir on whiteCir on bigRedCir) above
-blackRec above
-brownRec above
-greenRec
+    val smallRedCir = filledCircle(factor * 5, Color.red, factor * 2, Color.black)
+    val whiteCir = filledCircle(factor * 10, Color.white, factor * 2, Color.black)
+    val bigRedCir = filledCircle(factor * 15, Color.red, factor * 2, Color.black)
+
+    val image = smallRedCir on whiteCir on bigRedCir above
+      blackRec above
+      brownRec above
+      greenRec
+    image.draw
+  }
 }
